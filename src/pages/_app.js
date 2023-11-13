bbdb6040ebe79bba716b5b9fe41210c3827bfcd8
Ariling <97501226+Ariling@users.noreply.github.com>
2023-11-13 17:03:14 +0900

    feat & refactor  : gif에 따른 페이지 구성 구현 & 일부 코드들 수정

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,15 @@
+import Nav from "@/components/util/\bNav";
 import "@/styles/globals.css";
+import { useRouter } from "next/router";
 import { RecoilRoot } from "recoil";
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
   return (
     <RecoilRoot>
+      {router.pathname === "/" || router.pathname === "/waiting" ? null : (
+        <Nav />
+      )}
       <Component {...pageProps} />
     </RecoilRoot>
   );
