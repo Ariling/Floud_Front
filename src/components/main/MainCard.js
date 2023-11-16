c7d998555cad90da9350c5d590b9436873ae0ff9
minibxx <alsdbqls10@duksung.ac.kr>
2023-11-16 23:20:15 +0900

    feat : postCard 페이지 추가 및 컴포 연결

--- src/components/main/MainCard.js
+++ src/components/main/MainCard.js
@@ -10,8 +10,26 @@ import notWriteCloud from "@/img/main/notWriteCloud.png";
 import Image from "next/image.js";
 import PopupModalBtn from "../util/PopupModalBtn.js";
 import PostCard from "./PostCard.js";
+import MyRetroList from "@/components/Retro/MyRetroList";
+import { useRouter } from "next/router";
 
 const MainCard = () => {
+  const router = useRouter();
+  // test용 data
+  const date = 16;
+  const month = "Oct";
+  const title = "알잘딱깔센 알찬 하루 보냈다.";
+  const memoir_id = 12;
+  
+  const onCardClick = (memoirId) => {
+    router.push(`/memoir/${memoirId}`);
+  };
+
+  const onEditClick = (memoirId) => {
+    console.log("onEditClick :", memoirId);
+  };
+
+
   const dayInfo = useRecoilValue(weeklyDayAtom);
   const [tomorrowDay, setTomorrowDay] = useState(
     `${dayjs().add(1, "day").format("YYYY-MM-DD")} 06:00:00`
@@ -65,7 +83,7 @@ const MainCard = () => {
             </div>
             <button
               className="my-[26px] h-[50px] w-[292px] rounded-[15px] bg-[#66A4FF] text-[20px] font-bold tracking-[-1.2px] text-white"
-              onClick={() => alert("작성가능!")}
+              onClick={() => router.push(`/memoir/write`)}
             >
               회고록 작성하기
             </button>
@@ -107,7 +125,14 @@ const MainCard = () => {
         )}
       </div>
       {/* 해당 부분은 나중에 위쪽으로 옮겨질 예정 */}
-      <PostCard />
+      <MyRetroList
+        memoirId={memoir_id}
+        date={date}
+        month={`${month}.`}
+        todayTitle={title}
+        onCardClick={onCardClick}
+        onEditClick={onEditClick}
+      />
     </>
   );
 };
