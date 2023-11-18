2ebd1a953c343df6582c8b8597b29bb588b6f459
minibxx <alsdbqls10@duksung.ac.kr>
2023-11-18 23:52:07 +0900

    test

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -7,6 +7,7 @@ export default function Document() {
         <meta charset="UTF-8" />
         <link rel="icon" type="image" href="/Floudcloud.png" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
         <title>Floud</title>
       </Head>
       <body>
